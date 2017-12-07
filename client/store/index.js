8b6e117e81da5c0185c85179e49d2555f3f29bd4
Tess Monical <tess.monical@gmail.com>
2017-12-07 10:28:28 -0600

    add the podfics stuff to the store as a whole

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,14 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import user from './user'
 
-const reducer = combineReducers({user})
+import user from './user'
+import podfics from './podfics'
+
+const reducer = combineReducers({
+  user,
+  podfics
+})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
@@ -13,3 +18,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './podfics'
