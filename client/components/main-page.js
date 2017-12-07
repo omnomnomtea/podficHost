d66bb0328fc050e4511175e13a1b25833b0f2d00
Tess Monical <tess.monical@gmail.com>
2017-12-07 14:24:56 -0600

    adds main page

---
+++ client/components/main-page.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import SinglePodfic from './single-podfic'
+import {fetchRecentPodfics} from '../store'
+
+class MainPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loaded: false
+    }
+  }
+
+  loadOnce () {
+    if (!this.state.loaded) this.props.loadPodfics()
+    this.setState({loaded: true})
+  }
+
+  componentDidMount () {
+    const { podfics } = this.props;
+    this.loadOnce()
+  }
+  componentWillReceiveProps (newProps) {
+    this.loadOnce()
+  }
+  render () {
+    const {podfics} = this.props
+    if (!podfics) return <div className="loading" />
+    return (
+    <div>
+      {
+        podfics.map(podfic => {
+          return (
+            <SinglePodfic key={podfic.id} podfic={podfic} />
+          )
+        })
+      }
+    </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state, ownProps) => {
+
+  return {
+    podfics: state.podfics,
+  }
+}
+
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    loadPodfics (numPerPage, pageNum) {
+      numPerPage = Number(numPerPage || ownProps.match.params.numPerPage || 20)
+      pageNum = Number(pageNum || ownProps.match.params.pageNum || 1)
+      console.log('numPerPage', numPerPage)
+      console.log('pageNum', pageNum)
+
+      dispatch(fetchRecentPodfics(numPerPage, (pageNum - 1) * numPerPage))
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(MainPage)
