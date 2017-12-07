94f822c439f4700509f4c6d0255145a0d12dc21a
Tess Monical <tess.monical@gmail.com>
2017-12-06 23:51:46 -0600

    actually use those api routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 module.exports = router
 
+
 router.use('/users', require('./users'))
+router.use('./podfics', require('./podfics'))
 router.use('/tags/', require('./tags'))
 router.use('/pairings', require('./pairings'))
 router.use('/characters', require('./characters'))
 router.use('/audios/', require('./audios'))
 router.use('/fandoms/', require('./fandoms'))
-
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
