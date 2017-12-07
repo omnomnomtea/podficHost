4e7b23acab98a69c3590860c2174f65c128440af
Tess Monical <tess.monical@gmail.com>
2017-12-07 14:26:04 -0600

    convert single-podfic into a dumb component

--- client/components/single-podfic.js
+++ client/components/single-podfic.js
@@ -1,69 +1,52 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {fetchPodfic} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-class SinglePodfic extends React.Component {
-  constructor(){
-    super()
-    this.handleDownload = this.handleDownload.bind(this)
-  }
-  componentDidMount () {
-    const { podfic } = this.props;
-    if (!podfic) this.props.loadPodfic()
-  }
-  componentWillReceiveProps (newProps) {
-    if (!this.props.podfic && !newProps.podfic) this.props.loadPodfic()
-  }
-  handleDownload(audio){
-    window.open(audio.audioUrl)
-  }
-
-  render () {
-    const {podfic} = this.props
-    if (!podfic) return <div className="loading" />
-    return (
-      <div className="single-podfic">
-        <span className="podfic-title">{podfic.title}</span> <strong>by</strong> <span className="podfic-authors">
-            {podfic.users.map(user => user.username).join(', ')}
-          </span>
-        <div className="podfic-image">
-          <img src={podfic.image || 'http://via.placeholder.com/200x200'} />
-        </div>
-        <div className="description">
-          <strong>Description: </strong>
-          {podfic.description}
-        </div>
-          {
-            podfic.audios.map(audio=> (
-              <div key={audio.key}>
-                {audio.title} ({audio.fileType})
-                <button className="download-button" onClick={() => this.handleDownload(audio)}>download</button>
-              </div>
-            ))
-          }
-        </div>
-    )
-  }
+// helper functions
+const handleDownload = (audio) => {
+  window.open(audio.audioUrl)
 }
+// main function
+const SinglePodfic = (props) => {
+  const { podfic } = props
+  if (!podfic) return <div className="loading" />
+  return (
+    <div className="single-podfic">
+      <Link to={`/podfics/${podfic.id}`}>{podfic.title}</Link> <strong>by</strong> <span className="podfic-authors">
+        {podfic.users.map(user => user.username).join(', ') || 'Orphan Account'}
+      </span>
+      <div className="podfic-image">
+        <img src={podfic.image || 'http://via.placeholder.com/200x200'} />
+      </div>
+      <div className="description">
+        <strong>Description: </strong>
+        {podfic.description}
+      </div>
+      {
+        podfic.audios.map(audio => (
+          <div key={audio.id}>
+            {audio.title} ({audio.fileType})
+              <button className="download-button" onClick={() => handleDownload(audio)}>download</button>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
 
 /**
  * CONTAINER
  */
 const mapState = (state, ownProps) => {
-  const id = Number(ownProps.match.params.id)
   return {
-    podfic: state.podfics.find(podfic => podfic.id === id),
+    podfic: ownProps.podfic,
   }
 }
 
+
 const mapDispatch = (dispatch, ownProps) => {
-  const id = Number(ownProps.match.params.id)
-  return {
-    loadPodfic () {
-      dispatch(fetchPodfic(id))
-    }
-  }
+  return {}
 }
 
 export default connect(mapState, mapDispatch)(SinglePodfic)
