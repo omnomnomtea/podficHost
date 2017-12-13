ed187f42a37d9b7029ade65fd22b1a32b65de7de
Tess Monical <tess.monical@gmail.com>
2017-12-13 10:31:58 -0600

    conditionally renders now

--- client/components/extra-info.js
+++ client/components/extra-info.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 const ExtraInfo = (props) => {
   const { podfic } = props
   return (
-    <div>
-      <Link to={podfic.textUrl}>Original Text</Link> by {podfic.textAuthor}
+    <div className="additional-info">
+    <h4>Additional Information:</h4>
+      {podfic.textUrl && <React.Fragment><a href={podfic.textUrl}>Original Text</a></React.Fragment>}
+      {podfic.textAuthor && <React.Fragment> by {podfic.textAuthor}</React.Fragment>}
     </div>
   )
 }
