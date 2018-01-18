b4890bedc0c4cddd1513f14440e9ebb5e30bb952
Tess Monical <tess.monical@gmail.com>
2018-01-18 16:38:48 -0600

    basic 'ORM' functionality to connect to Neo4j (needs much more security)

--- server/db/index.js
+++ server/db/index.js
@@ -24,7 +24,7 @@ const allowedConnectionTypes = [
   'HAS_PAIRING',
 ];
 
-const createNode = async (name, type) => {
+const createNode = async (type) => {
   if (!allowedNodeTypes.includes(type)) {
     throw new Error(`Disallowed node type: ${type}`);
   }
@@ -33,7 +33,7 @@ const createNode = async (name, type) => {
   const results = await session.run(
     `
     CREATE
-    (newNode:${type} {name: $name})
+    (newNode:${type})
     RETURN newNode
     `,
     { name },
@@ -46,12 +46,10 @@ const createConnection = async (nodeOrId1, type, nodeOrId2) => {
   if (!allowedConnectionTypes.includes(type)) {
     throw new Error(`Disallowed connection type: ${type}`);
   }
-
   let id1;
   let id2;
-
   // this will be used in string interpolation so having this num be
-  // a string is fine-- converting to a number could result in overflow
+  // a string is fine-- converting to a number could result in overflow since neo4j allows larger numbers than javascipt
   if (typeof nodeOrId1 === 'number') id1 = nodeOrId1;
   else id1 = nodeOrId1.identity.toString();
   if (typeof nodeOrId2 === 'number') id2 = nodeOrId2;
@@ -60,14 +58,60 @@ const createConnection = async (nodeOrId1, type, nodeOrId2) => {
   const session = driver.session();
   const results = await session.run(
     `
-    MATCH (node1) WHERE ID(node1)=${id1}
-    MATCH (node2) WHERE ID(node2)=${id2}
+    MATCH (node1) WHERE ID(node1)=$id1
+    MATCH (node2) WHERE ID(node2)=$id2
     CREATE
     (node1)-[newConnection:${type}]->(node2)
-    RETURN newConnection
+    RETURN newConnection;
     `,
-    {},
+    {id1, id2},
   );
   // uses built-in getter to get the record in a nicer format
   return results.records[0].get(0);
 };
+
+const deleteNode = (nodeOrId, options) => {
+  let id
+  if (typeof nodeOrId === 'number') id = nodeOrId;
+  //safer than toNumber since Neo4j numbers can be larger than JS numbers
+  else id = nodeOrId.identity.toString();
+
+  let force
+  if (!options) force = false
+  else force = options.force
+
+  const session = driver.session();
+
+  let query = `MATCH (n) WHERE ID(n)=${id} DELETE n;`
+  if (force) { // add DETACH (deletes all connections if node is connected)
+    query = `MATCH (n) WHERE ID(n)=${id} DETACH DELETE n;`
+  }
+
+  return session.run(query)
+    .then(() => true)
+    .catch((err) => {
+      console.error(err)
+      return false
+    })
+}
+
+const deleteConnection = (connectionId, options) => {
+  if (typeof connectionId !== 'number') connectionId = connectionId.identity.toString()
+
+  const session = driver.session();
+  let query = `MATCH [n] WHERE ID[n]=${connectionId} DELETE n;`
+  return session.run(query)
+  .then(() => true)
+  .catch((err) => {
+    console.error(err)
+    return false
+  })
+}
+
+
+module.exports = {
+  createNode,
+  deleteNode,
+  createConnection,
+  deleteConnection,
+}
